89c498e722cec39491704f14b422631be2b30902
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2025-01-22 14:54:55 +0500

    chore(nest): болванка для модулей

--- backend/src/core/app.module.ts
+++ backend/src/core/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '@/shared/prisma';
+import { AuthModule } from '@/modules/auth/auth.module';
+import { CartModule } from '@/modules/cart/cart.module';
+import { ProductModule } from '@/modules/product/product.module';
+import { StoryModule } from '@/modules/story/story.module';
+import { UserModule } from '@/modules/user/user.module';
 
 @Module({
-	providers: [PrismaModule],
+	providers: [
+		PrismaModule,
+		AuthModule,
+		CartModule,
+		ProductModule,
+		StoryModule,
+		UserModule,
+	],
 })
 export class AppModule {}
