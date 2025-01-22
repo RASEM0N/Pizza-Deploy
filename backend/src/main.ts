ec237002324f09646bbbf72c5d09435cb9ac186c
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2025-01-22 15:11:34 +0500

    chore(back): add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/core/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+// https://docs.nestjs.com/openapi/introduction
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	const doc = new DocumentBuilder()
+		.setTitle('Pizza | Api documentation')
+		.setVersion('0.0.0')
+		.build();
+
+	SwaggerModule.setup('api/docs', app, () => SwaggerModule.createDocument(app, doc));
+
 	await app.listen(3000);
 }
+
 bootstrap();
