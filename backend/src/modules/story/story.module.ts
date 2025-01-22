cacb5cc3bfb50dc1949eae442376dc9812ffaeca
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2025-01-22 15:17:03 +0500

    chore(back): контроллеры болванки

--- backend/src/modules/story/story.module.ts
+++ backend/src/modules/story/story.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StoryService } from './story.service';
+import { StoryController } from './story.controller';
 
 @Module({
 	providers: [StoryService],
 	exports: [StoryService],
+	controllers: [StoryController]
 })
 export class StoryModule {}
