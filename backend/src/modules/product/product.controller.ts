6f0c1cb23a94fa5b81efa1c806772e2b1f456f75
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2025-01-23 10:53:20 +0500

    feat(back): product

--- backend/src/modules/product/product.controller.ts
+++ backend/src/modules/product/product.controller.ts
@@ -1,6 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Product } from '@prisma/client';
+import { ProductService } from './product.service';
 
 @ApiTags('Product')
-@Controller()
-export class ProductController {}
+@Controller('product')
+export class ProductController {
+	constructor(private readonly productService: ProductService) {}
+
+	@Get('search')
+	get(@Query('query') query: string): Promise<Product[]> {
+		return this.productService.get(query);
+	}
+}
--- backend/src/modules/product/product.service.ts
+++ backend/src/modules/product/product.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@/shared/prisma';
+import { Product } from '@prisma/client';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+	constructor(private readonly prisma: PrismaService) {}
+
+	get(query: string): Promise<Product[]> {
+		return this.prisma.product.findMany({
+			where: { name: { contains: query, mode: 'insensitive' } },
+			take: 5,
+		});
+	}
+}
