5fec8c991e2a6362bcb5b9c45e060a8541e2270b
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2025-01-24 06:32:11 +0500

    fix(back): микроправки

--- backend/src/shared/cookie/cookie.decorator.ts
+++ backend/src/shared/cookie/cookie.decorator.ts
@@ -1,7 +1,12 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { BadRequestException, createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 
 export const Cookie = createParamDecorator((data: string, ctx: ExecutionContext) => {
-	const req = ctx.switchToHttp().getRequest<Request>();
-	return req.cookies[data];
+	const value = ctx.switchToHttp().getRequest<Request>().cookies[data];
+
+	if (!value) {
+		throw new BadRequestException(`${data} cookie is empty`)
+	}
+
+	return value;
 });
