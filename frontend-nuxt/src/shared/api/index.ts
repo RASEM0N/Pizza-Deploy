35b8484d561f1e6138a84a6d9b1f6f7d36a4d3e8
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2025-01-26 23:27:53 +0500

    refactor(nuxt): part 0.001

--- frontend-nuxt/src/shared/api/index.ts
+++ frontend-nuxt/src/shared/api/index.ts
@@ -1,9 +1,16 @@
-// @TODO тоже самое для $fetch
-
 export const useApiFetch = <T>(...[url, options]: Parameters<typeof useFetch<T>>) => {
 	const config = useRuntimeConfig();
 
 	return useFetch<T>(url, {
+		...options,
+		baseURL: config.public.baseUrl,
+	});
+};
+
+export const $apiFetch = <T>(...[url, options]: Parameters<typeof $fetch<T>>) => {
+	const config = useRuntimeConfig();
+
+	return $fetch<T>(url, {
 		...options,
 		baseURL: config.public.baseUrl,
 	});
--- frontend-nuxt/src/shared/ui/sort-popup/SortPopup.vue
+++ frontend-nuxt/src/shared/ui/sort-popup/SortPopup.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
+// @todo
 
-interface Props {
+interface Props {}
 
-}
-
-const { t } = useI18n()
-const {} = defineProps()
+const { t } = useI18n();
+const {} = defineProps();
 </script>
 <template>
 	<UiPopover>
@@ -13,7 +12,7 @@ const {} = defineProps()
 			<div
 				class="inline-flex items-center gap-1 bg-gray-50 px-5 h-[52px] rounded-2xl cursor-pointer"
 			>
-				<IconArrowUpDown class="w-4 h-4"/>
+				<IconArrowUpDown class="w-4 h-4" />
 				<b>{{ t('sort.name') }}</b>
 				<b class="text-primary">{{ t('sort.popular') }}</b>
 			</div>
